2496e4683f38d787614be335b106a4e3902dedc5
divyuk <div14kes@gmail.com>
2023-08-20 14:38:11 +0530

    Green 🟢 Implemented the Loading Functionality

--- src/tests/Loading.test.tsx
+++ src/tests/Loading.test.tsx
@@ -1,10 +1,12 @@
 import { render } from "@testing-library/react";
 import Loading from "../components/Loading";
-import { describe, test } from "vitest";
+import { describe, test, expect } from "vitest";
 import "@testing-library/jest-dom";
 
 describe("Loading Component", () => {
   test("renders a loading spinner", () => {
     const { container } = render(<Loading />);
+    const loadingSpinner = container.querySelector(".animate-spin");
+    expect(loadingSpinner).toBeInTheDocument();
   });
 });
