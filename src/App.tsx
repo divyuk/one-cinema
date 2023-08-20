50d1dbc3240289e58f984be056d2ba445af0036c
divyuk <div14kes@gmail.com>
2023-08-20 15:35:19 +0530

    Provider , Providing the state management } Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListPage from "./pages/ListPage";
 import DetailPage from "./pages/DetailPage";
-
+import { Provider } from "react-redux";
+import { store } from "./Store/store";
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <div>
-          <Routes>
-            {/* Route for the ListPage which is the Home page */}
-            <Route index element={<ListPage />} />
-            {/* Route for the DetailsPage */}
-            <Route path="/movie/:id" element={<DetailPage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Routes>
+              {/* Route for the ListPage which is the Home page */}
+              <Route index element={<ListPage />} />
+              {/* Route for the DetailsPage */}
+              <Route path="/movie/:id" element={<DetailPage />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
