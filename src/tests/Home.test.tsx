cd239ccb7b09acffab807aeb0d90b4f7870dc27c
divyuk <div14kes@gmail.com>
2023-08-20 14:33:24 +0530

    Green 🟢 : Implements the Home Component, passed

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,8 +1,7 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
 import Home from "../components/Home";
-import { describe, test } from "vitest";
+import { describe, test, expect } from "vitest";
 import { MemoryRouter } from "react-router-dom";
 
 describe("Home Component", () => {
@@ -12,5 +11,7 @@ describe("Home Component", () => {
         <Home />
       </MemoryRouter>
     );
+    const homeImage = getByAltText("home");
+    expect(homeImage).toBeInTheDocument();
   });
 });
