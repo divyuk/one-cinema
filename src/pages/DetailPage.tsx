1ce90abd3e8f1912deec5a07de6c43fb18fce413
divyuk <div14kes@gmail.com>
2023-08-20 13:29:58 +0530

    List Page and the Details Page 📄📄

---
+++ src/pages/DetailPage.tsx
@@ -0,0 +1,5 @@
+import MovieDetail from "../components/MovieDetail";
+
+export default function DetailPage() {
+  return <MovieDetail />;
+}
---
+++ src/pages/ListPage.tsx
@@ -0,0 +1,7 @@
+import Search from "../components/Search";
+
+function ListPage() {
+  return <Search />;
+}
+
+export default ListPage;
